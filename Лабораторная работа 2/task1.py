7469e9be52c78e5e339f72f56fe52126f794c7b8
Homiak1 <147329285+Homiak1@users.noreply.github.com>
2023-11-17 16:27:08 +0300

    Update task1.py

--- Лабораторная работа 2/task1.py
+++ Лабораторная работа 2/task1.py
@@ -3,9 +3,9 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 average_capital = money_capital + salary - spend
-total_month = 1
+total_month = 0
 while average_capital + salary > spend:
-    spend = spend * (increase + 1)
     average_capital = (average_capital + salary) - spend
+    spend = spend * (increase + 1)
     total_month += 1
 print("Количество месяцев, которое можно протянуть без долгов:", total_month)
